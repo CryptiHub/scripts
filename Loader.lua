23f681cc7878c2def3498e49de037930536963bc
CryptiHub <theoperationnamedcryptic@gmail.com>
2025-10-02 21:18:01 +0930

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local tree = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/CryptiHub/scripts/git/trees/"..game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api.github.com/repos/CryptiHub/scripts/branches/main")).commit.commit.tree.sha.."?recursive=1"))
 for _, scrip in ipairs(tree.tree) do
-    if scrip.type == "blob" and strip.path ~= "Loader.lua" and string.split(string.split(scrip.path:lower(), ".lua")[1]:lower(),game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower())[1] ~= game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower() then
+    if scrip.type == "blob" and scrip.path ~= "Loader.lua" and string.split(string.split(scrip.path:lower(), ".lua")[1]:lower(),game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower())[1] ~= game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower() then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptiHub/scripts/main/"..scrip.path))()
     end
 end

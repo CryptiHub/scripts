b39ee4872952b5ef6e769af80707009c57d1bacf
CryptiHub <theoperationnamedcryptic@gmail.com>
2025-10-02 22:27:22 +0930

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,5 +2,7 @@ local tree = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://api
 for _, scrip in ipairs(tree.tree) do
     if scrip.type == "blob" and scrip.path ~= "Loader.lua" and string.split(string.split(scrip.path:lower(), ".lua")[1]:lower(),game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower())[1] ~= game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name:lower() then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/CryptiHub/scripts/main/"..scrip.path))()
+    else
+        print("game not supported")
     end
 end
